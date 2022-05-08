05fb161abdf8cbac57b3719e4cf5b4cafe8cc356
Tintenpatron <p429344@gmail.com>
2022-05-08 23:02:19 +0200

    added theme color

--- assets/php/templates/og.php
+++ assets/php/templates/og.php
@@ -2,5 +2,4 @@
 <meta property="og:description" content="<?= $lang['og_desc']?>">
 <meta property="og:image" content="https://nevar.eu/assets/img/favicon.webp">
 <meta property="og:type" content="website">
-<meta property="og:color" content="#5865F2">
 <meta name="theme-color" content="#5865F2">
