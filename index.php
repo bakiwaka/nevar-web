fe4d2f31e4aceca404ac9a775e81e4eff0275bc7
Tintenpatron <p429344@gmail.com>
2022-06-09 20:15:02 +0200

    elimante render blocking ressources

--- index.php
+++ index.php
@@ -14,19 +14,15 @@ include_once './assets/php/functions/switchLang.php';
 
     <!-- OG Tags -->
     <?php include_once './assets/php/templates/og.php' ?>
-
     <!-- Favicons -->
     <link href="assets/img/favicon.webp" rel="icon">
     <link href="assets/img/favicon.webp" rel="apple-touch-icon">
-    <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
     <!-- Vendor CSS Files -->
     <link href="assets/vendor/aos/aos.css" rel="stylesheet">
     <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
     <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
     <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
     <!-- Main CSS File -->
     <link href="assets/css/style.css" rel="stylesheet">
 </head>
@@ -500,6 +496,9 @@ include_once './assets/php/functions/switchLang.php';
 
 <!-- Main JS File -->
 <script src="assets/js/main.js"></script>
+<link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
 </body>
 <link rel="stylesheet" href="/assets/css/cookies.css" />
 <script src="/assets/js/cookies.js"></script>
